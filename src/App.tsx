08ac17b8a80b620bfb6615e34d869319a56782d3
mark.avvakumov <maavvakumov@gmail.com>
2024-01-11 16:07:17 +0700

    Add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
+import { lazy } from 'react'
+import {
+  Route,
+  Outlet,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Navigate
+} from 'react-router-dom'
 import './App.css'
-import Home from './pages/Home'
 
-const App = () => {
-  return <Home />
+const Layout = lazy(() => import('./components/Layout'))
+const Home = lazy(() => import('./pages/Home'))
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path='/*'
+      element={
+        <Layout>
+          <Outlet />
+        </Layout>
+      }>
+      <Route index element={<Navigate replace to='home' />} />
+      <Route path='home' element={<Home />} />
+    </Route>
+  )
+)
+
+function App() {
+  return <RouterProvider router={router} />
 }
 
 export default App
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,12 @@
-import Header from '../components/Header'
 import Categories from '../components/Categories'
-import Footer from '../components/Footer'
 import Products from '../components/Products'
 
 const Home = () => {
   return (
-    <div>
-      <Header />
+    <>
       <Categories />
       <Products />
-      <Footer />
-    </div>
+    </>
   )
 }
 export default Home
