603ec2009e8ba6b7aca021cebc6b22e77409cecd
mark.avvakumov <maavvakumov@gmail.com>
2024-01-11 16:06:48 +0700

    Add basic layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,18 @@
+import Footer from './Footer'
+import Header from './Header'
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
