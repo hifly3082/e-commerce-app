e945aeb2aae8630098e6e93c9bb50edebf3fa8f3
mark.avvakumov <maavvakumov@gmail.com>
2024-01-12 16:33:50 +0700

    Add Product

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,20 +1,158 @@
 import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
+
 import { useProductFetch } from '../api/fetch'
+import SmallSpinner from '../components/Spinner'
 
 const Container = styled.div``
+
+const Wrapper = styled.div`
+  padding: 3rem;
+  display: flex;
+`
+
+const ImgContainer = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: center;
+`
+
+const Image = styled.img`
+  height: 50vh;
+  object-fit: cover;
+`
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0 3rem;
+`
+
+const Title = styled.h1`
+  font-weight: 200;
+`
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`
+
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`
+
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+`
+
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`
+
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`
+
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`
+
+const FilterSizeOption = styled.option``
+
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`
+
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 5px;
+`
+
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+
+  &:hover {
+    background-color: #f8f4f4;
+  }
+`
 
 const Product: React.FC = () => {
   const { id } = useParams()
   const { product, loading, error } = useProductFetch(id)
-  console.log(product)
 
-  if (loading) return <div>loading</div>
+  if (loading) return <SmallSpinner />
 
   return (
     <Container>
-      {id}
-      {product.price}
+      <Wrapper>
+        <ImgContainer>
+          <Image src={product.image} />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>{product.title}</Title>
+          <Desc>{product.description}</Desc>
+          <Price>$ {product.price}</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color='black' />
+              <FilterColor color='darkblue' />
+              <FilterColor color='gray' />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <Button>-</Button>
+              <Amount>1</Amount>
+              <Button>+</Button>
+            </AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
     </Container>
   )
 }
