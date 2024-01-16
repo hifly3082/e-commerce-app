a53c4c941d932554652354a8c5e664e10987d9dd
mark.avvakumov <maavvakumov@gmail.com>
2024-01-16 16:16:28 +0700

    Add RTK Query

---
+++ src/features/api/storeApi.ts
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const BASE_URL = 'https://api.escuelajs.co/api/v1'
+
+export const storeApi = createApi({
+  reducerPath: 'storeApi',
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  endpoints: (builder) => ({
+    getCategories: builder.query({
+      query: () => `/categories`
+    }),
+    getProducts: builder.query({
+      query: () => `/products`
+    }),
+    getProduct: builder.query({
+      query: (id) => `/products/${id}`
+    })
+  })
+})
+
+export const {
+  useGetCategoriesQuery,
+  useGetProductsQuery,
+  useGetProductQuery
+} = storeApi
