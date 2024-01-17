d99658c785aac19cf3aa22b7ca837d78770ec7aa
mark.avvakumov <maavvakumov@gmail.com>
2024-01-17 16:36:42 +0700

    Add image slider

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,8 +1,13 @@
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { BsArrowLeftCircleFill } from 'react-icons/bs'
 import styled from 'styled-components'
 
 import SmallSpinner from '../components/Spinner'
 import { useGetProductQuery } from '../features/api/storeApi'
+import { addItemToCart } from '../features/cart/cartSlice'
+import { IProductItem } from '../types/types'
 
 const Container = styled.div``
 
@@ -15,11 +20,33 @@ const ImgContainer = styled.div`
   display: flex;
   flex: 1;
   justify-content: center;
+  position: relative;
 `
 
 const Image = styled.img`
   height: 50vh;
   object-fit: cover;
+`
+
+const Arrow = styled(BsArrowLeftCircleFill)`
+  position: absolute;
+  width: 2rem;
+  height: 2rem;
+  top: 50%;
+  color: #fff;
+  cursor: pointer;
+  &:hover {
+    color: #444;
+  }
+`
+
+const RightArrow = styled(Arrow)`
+  left: 1rem;
+`
+
+const LeftArrow = styled(Arrow)`
+  right: 1rem;
+  transform: scaleX(-1);
 `
 
 const InfoContainer = styled.div`
@@ -37,53 +64,21 @@ const Desc = styled.p`
 
 const Price = styled.span`
   font-weight: 100;
-  font-size: 40px;
+  font-size: 2.4rem;
 `
-
-const FilterContainer = styled.div`
-  width: 50%;
-  margin: 30px 0px;
-  display: flex;
-  justify-content: space-between;
-`
-
-const Filter = styled.div`
-  display: flex;
-  align-items: center;
-`
-
-const FilterTitle = styled.span`
-  font-size: 20px;
-  font-weight: 200;
-`
-
-const FilterColor = styled.div`
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-  background-color: ${(props) => props.color};
-  margin: 0px 5px;
-  cursor: pointer;
-`
-
-const FilterSize = styled.select`
-  margin-left: 10px;
-  padding: 5px;
-`
-
-const FilterSizeOption = styled.option``
 
 const AddContainer = styled.div`
   width: 50%;
   display: flex;
   align-items: center;
   justify-content: space-between;
+  margin-top: 1rem;
 `
 
 const AmountContainer = styled.div`
   display: flex;
   align-items: center;
-  font-weight: 700;
+  font-weight: 500;
 `
 
 const Amount = styled.span`
@@ -94,24 +89,54 @@ const Amount = styled.span`
   display: flex;
   align-items: center;
   justify-content: center;
-  margin: 0px 5px;
+  margin: 0 1rem;
 `
 
 const Button = styled.button`
-  padding: 15px;
-  border: 2px solid teal;
-  background-color: white;
-  cursor: pointer;
-  font-weight: 500;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 0.5rem;
 
+  cursor: pointer;
+  transition: all 0.4s ease-in-out;
   &:hover {
-    background-color: #f8f4f4;
+    background-color: #ccc;
   }
 `
 
+const AddButton = styled(Button)`
+  margin-top: 1rem;
+`
+
 const Product: React.FC = () => {
+  const [amount, setAmount] = useState(1)
+  const [index, setIndex] = useState(0)
   const { id } = useParams()
+  const dispatch = useDispatch()
+
   const { data: product, isLoading, isError } = useGetProductQuery(id)
+
+  const nextIndex = () => {
+    setIndex(index === product.images.length - 1 ? 0 : index + 1)
+  }
+
+  const prevIndex = () => {
+    setIndex(index === 0 ? product.images.length - 1 : index - 1)
+  }
+
+  const handleDecreaseAmount = () => {
+    if (amount > 1) {
+      setAmount(amount - 1)
+    }
+  }
+
+  const handleIncreaseAmount = () => {
+    setAmount(amount + 1)
+  }
+  const handleAddItem = (item: IProductItem) => () => {
+    dispatch(addItemToCart({ ...item, quantity: amount }))
+  }
 
   if (isLoading) return <SmallSpinner />
 
@@ -123,38 +148,22 @@ const Product: React.FC = () => {
     <Container>
       <Wrapper>
         <ImgContainer>
-          <Image src={product.images[0]} />
+          <RightArrow onClick={prevIndex} />
+          <Image src={product.images[index]} />
+          <LeftArrow onClick={nextIndex} />
         </ImgContainer>
         <InfoContainer>
           <Title>{product.title}</Title>
           <Desc>{product.description}</Desc>
           <Price>$ {product.price}</Price>
-          <FilterContainer>
-            <Filter>
-              <FilterTitle>Color</FilterTitle>
-              <FilterColor color='black' />
-              <FilterColor color='darkblue' />
-              <FilterColor color='gray' />
-            </Filter>
-            <Filter>
-              <FilterTitle>Size</FilterTitle>
-              <FilterSize>
-                <FilterSizeOption>XS</FilterSizeOption>
-                <FilterSizeOption>S</FilterSizeOption>
-                <FilterSizeOption>M</FilterSizeOption>
-                <FilterSizeOption>L</FilterSizeOption>
-                <FilterSizeOption>XL</FilterSizeOption>
-              </FilterSize>
-            </Filter>
-          </FilterContainer>
           <AddContainer>
             <AmountContainer>
-              <Button>-</Button>
-              <Amount>1</Amount>
-              <Button>+</Button>
+              <Button onClick={handleDecreaseAmount}>-</Button>
+              <Amount>{amount}</Amount>
+              <Button onClick={handleIncreaseAmount}>+</Button>
             </AmountContainer>
-            <Button>ADD TO CART</Button>
           </AddContainer>
+          <AddButton onClick={handleAddItem(product)}>ADD TO CART</AddButton>
         </InfoContainer>
       </Wrapper>
     </Container>
