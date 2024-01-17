840fd1c6e8cdf276a85f62b70e7ac2dea023bf0b
mark.avvakumov <maavvakumov@gmail.com>
2024-01-17 14:53:22 +0700

    Update CartPage

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,8 +1,9 @@
-import styled from 'styled-components'
-import { useSelector, useDispatch } from 'react-redux'
-import { cartSlice } from '../features/cart/cartSlice'
-import { ICartItem, IRootState } from '../types/types'
 import { useNavigate } from 'react-router'
+import { useSelector, useDispatch } from 'react-redux'
+import styled from 'styled-components'
+
+import { ICartItem, IRootState } from '../types/types'
+import { cartSlice } from '../features/cart/cartSlice'
 
 const Container = styled.div`
   display: flex;
@@ -21,9 +22,9 @@ const Header = styled.div`
 
 const ItemContainer = styled.div`
   display: flex;
-  justify-content: space-between;
   align-items: center;
   margin-bottom: 10px;
+  justify-content: space-between;
 `
 
 const ItemImage = styled.img`
@@ -34,18 +35,19 @@ const ItemImage = styled.img`
   cursor: pointer;
 `
 
-const ItemDetails = styled.div`
-  flex: 1;
+const Quantity = styled.div`
+  display: flex;
+  align-items: center;
   margin-left: 10px;
 `
 
 const ItemName = styled.h3`
   font-size: 1.2rem;
-  font-weight: 600;
+  font-weight: 500;
 `
 
 const ItemPrice = styled.p`
-  font-size: 1.2rem;
+  font-size: 1rem;
   font-weight: 600;
   color: #666;
 `
@@ -54,18 +56,50 @@ const ItemQuantity = styled.p`
   font-size: 1rem;
   font-weight: 400;
   color: #444;
+  margin-right: 10px;
+`
+
+const Button = styled.button`
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 5px;
+  margin-right: 5px;
+  cursor: pointer;
+  transition: all 0.4s ease-in-out;
+  &:hover {
+    background-color: #ccc;
+  }
+`
+
+const Checkout = styled(Button)`
+  background-color: teal;
+  color: #fff;
+`
+
+const Total = styled.div`
+  text-align: right;
+  font-size: 1rem;
+  font-weight: 600;
+  color: #666;
+  margin: 2rem 1rem 1rem;
 `
 
 const CartPage: React.FC = () => {
   const navigate = useNavigate()
   const dispatch = useDispatch()
   const cartState = useSelector((state: IRootState) => state.cart)
+  const cartQuantity = cartState.items.length
+  const totalSum = cartState.items.reduce(
+    (sum: number, item) => sum + item.price * item.quantity,
+    0
+  )
 
   const handleClearCart = () => {
     dispatch(cartSlice.actions.clearCart())
   }
 
-  const handleRemoveItem = (item: ICartItem) => {
+  const handleRemoveItem = (item: ICartItem) => () => {
     dispatch(cartSlice.actions.removeItemFromCart(item))
   }
 
@@ -73,16 +107,19 @@ const CartPage: React.FC = () => {
     navigate(`/product/${id}`)
   }
 
-  const totalSum = cartState.items.reduce(
-    (sum: number, item) => sum + item.price * item.quantity,
-    0
-  )
+  const handleIncrementQuantity = (item: ICartItem) => () => {
+    dispatch(cartSlice.actions.incrementQuantity(item))
+  }
 
-  return (
+  const handleDecrementQuantity = (item: ICartItem) => () => {
+    dispatch(cartSlice.actions.decrementQuantity(item))
+  }
+
+  return cartQuantity > 0 ? (
     <Container>
       <Header>
         <h2>Cart</h2>
-        <button onClick={handleClearCart}>Clear Cart</button>
+        <Button onClick={handleClearCart}>Clear Cart</Button>
       </Header>
       {cartState.items.map((item) => (
         <ItemContainer key={item.id}>
@@ -91,15 +128,27 @@ const CartPage: React.FC = () => {
             alt={item.title}
             onClick={handleOverview(item.id)}
           />
-          <ItemDetails>
-            <ItemName>{item.title}</ItemName>
-            <ItemPrice>${item.price}</ItemPrice>
-            <ItemQuantity>Quantity: {item.quantity}</ItemQuantity>
-          </ItemDetails>
-          <button onClick={() => handleRemoveItem(item)}>Remove</button>
+
+          <ItemName>{item.title}</ItemName>
+          <Quantity>
+            <Button onClick={handleDecrementQuantity(item)}>-</Button>
+            <ItemQuantity>{item.quantity}</ItemQuantity>
+            <Button onClick={handleIncrementQuantity(item)}>+</Button>
+          </Quantity>
+          <ItemPrice>${item.price * item.quantity}</ItemPrice>
+
+          <Button onClick={handleRemoveItem(item)}>Remove</Button>
         </ItemContainer>
       ))}
-      <p>Total Sum: ${totalSum}</p>
+      <hr />
+      <Total>Total Sum: ${totalSum}</Total>
+      <Checkout>Proceed to checkout</Checkout>
+    </Container>
+  ) : (
+    <Container>
+      <Header>
+        <h2>Cart is empty </h2>
+      </Header>
     </Container>
   )
 }
