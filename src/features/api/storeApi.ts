6d69b0f6bc06398e5a658064ba551e7b63031795
mark.avvakumov <maavvakumov@gmail.com>
2024-01-22 18:59:56 +0700

    Pass searchParams to queryFn

--- src/features/api/storeApi.ts
+++ src/features/api/storeApi.ts
@@ -2,24 +2,43 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const BASE_URL = 'https://api.escuelajs.co/api/v1'
 
+// export const storeApi = createApi({
+//   reducerPath: 'storeApi',
+//   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+//   endpoints: (builder) => ({
+//     getCategories: builder.query({
+//       query: () => `/categories`
+//     }),
+//     getProducts: builder.query({
+//       query: ({ title, priceMin, priceMax, categoryId }) => {
+//         let queryString = '/products/?'
+
+//         if (title) queryString += `&title=${title}`
+//         if (priceMin) queryString += `&price_min=${priceMin}`
+//         if (priceMax) queryString += `&price_max=${priceMax}`
+//         if (categoryId) queryString += `&categoryId=${categoryId}`
+
+//         return queryString
+//       }
+//     }),
+//     getProductById: builder.query({
+//       query: (id) => `/products/${id}`
+//     })
+//   })
+// })
+
 export const storeApi = createApi({
   reducerPath: 'storeApi',
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   endpoints: (builder) => ({
     getCategories: builder.query({
-      query: () => `/categories`
+      query: () => '/categories'
     }),
     getProducts: builder.query({
-      query: ({ title, priceMin, priceMax, categoryId }) => {
-        let queryString = '/products/?'
-
-        if (title) queryString += `&title=${title}`
-        if (priceMin) queryString += `&price_min=${priceMin}`
-        if (priceMax) queryString += `&price_max=${priceMax}`
-        if (categoryId) queryString += `&categoryId=${categoryId}`
-
-        return queryString
-      }
+      query: (searchParams) => ({
+        url: '/products/?',
+        params: searchParams
+      })
     }),
     getProductById: builder.query({
       query: (id) => `/products/${id}`
