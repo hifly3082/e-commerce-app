f93cbfe757420501bbc73eea1a1a1cea72a53d13
mark.avvakumov <maavvakumov@gmail.com>
2024-01-22 19:01:06 +0700

    Refactor with useSearchParams

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import styled from 'styled-components'
 
 import Search from '../components/Search'
 import Filters from '../components/Filters'
 import ProductsList from './ProductsList'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useSearchParams } from 'react-router-dom'
+import { useGetProductsQuery } from '../features/api/storeApi'
+import { useThrottle } from '../hooks/useThrottle'
 
 const PageContainer = styled.div`
   display: flex;
@@ -22,34 +24,38 @@ const Container = styled.div`
 
 const ProductsPage: React.FC = () => {
   const location = useLocation()
-  const categoryId = location.state?.categoryId || ''
-  const [searchParams, setSearchParams] = useState({
-    title: ''
+  // const categoryId = location.state?.categoryId || ''
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const title = searchParams.get('title') || ''
+  const priceMin = useThrottle(searchParams.get('priceMin') || '')
+  const priceMax = useThrottle(searchParams.get('priceMax') || '')
+  const categoryId = searchParams.get('categoryId') || ''
+  const {
+    data: products,
+    isLoading,
+    isError
+  } = useGetProductsQuery({
+    title,
+    price_min: priceMin,
+    price_max: priceMax,
+    categoryId
   })
-
-  const [filterParams, setFilterParams] = useState({
-    priceMin: '',
-    priceMax: '',
-    categoryId: ''
-  })
-
-  const handleSearchChange = (newSearchParams: any) => {
-    setSearchParams(newSearchParams)
-  }
-
-  const handleFiltersChange = (newFilterParams: any) => {
-    setFilterParams(newFilterParams)
-  }
 
   return (
     <PageContainer>
-      <Search onSearchChange={handleSearchChange} />
+      <Search setSearchParams={setSearchParams} />
       <Container>
         <Filters
-          defaultCategoryId={categoryId}
-          onFiltersChange={handleFiltersChange}
+          // defaultCategoryId={categoryId}
+          setSearchParams={setSearchParams}
         />
-        <ProductsList searchParams={searchParams} filterParams={filterParams} />
+        <ProductsList
+          // searchParams={searchParams}
+          products={products}
+          isLoading={isLoading}
+          isError={isError}
+        />
       </Container>
     </PageContainer>
   )
